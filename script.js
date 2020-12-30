9f37e5fb11d778fe0cc62433ba60b5e76a2bb0b0
Pradeep Raj <60281056+MPradeepRaj02@users.noreply.github.com>
2020-12-30 13:57:23 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function share() {
-    var message = $("input[name=Your Name]").val();
-    window.open(
-        "whatsapp://send?text="+message+"sent you a surprise with https://fauceter.in",'_blank'
-    );
-}
